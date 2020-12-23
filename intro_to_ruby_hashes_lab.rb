d03f83e14cdb566198910999951f7e9fd3bce3e8
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-23 00:30:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,8 +16,8 @@ def monopoly_with_second_tier
   
     my_hash = {
     :railroads => {
-      :pieces => 4
-      :rent_in_dollars => {}
+      :pieces => 4,
+      :rent_in_dollars => {},
       :names => {}
     }
   }
